06653df40d3ad3bf09f1ea513d8075839e294ce4
ManishShw <144468580+ManishShw@users.noreply.github.com>
2026-09-22 19:08:53 +0530

    clean up

--- ml_service/app/model_loader.py
+++ ml_service/app/model_loader.py
@@ -34,37 +34,3 @@ def load_models():
         "YIELD": yield_bundle,
         "BEEHAVE": beehave_pipeline
     }
-'''
-from pathlib import Path
-import joblib
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-MODEL_DIR = BASE_DIR / "models"
-
-
-def load_model(filename):
-    path = MODEL_DIR / filename
-
-    if not path.exists():
-        raise FileNotFoundError(
-            f"Model file not found: {path}"
-        )
-
-    return joblib.load(path)
-
-
-def load_models():
-
-    hive_1_model = load_model(
-        "hive_1_isolation_forest.joblib"
-    )
-
-    hive_2_model = load_model(
-        "hive_2_isolation_forest.joblib"
-    )
-
-    return {
-        "HIVE_01": hive_1_model,
-        "HIVE_02": hive_2_model
-    }'''
